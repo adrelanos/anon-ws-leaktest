5fb6ecbc3bd70ff34e0f7c75bb61345a565d004c
Patrick Schleizer <adrelanos@riseup.net>
2018-01-29 15:09:29 +0000

    update copyright

--- usr/lib/leaktest-workstation/tcp_test.py
+++ usr/lib/leaktest-workstation/tcp_test.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 ## This file is part of Whonix.
-## Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
+## Copyright (C) 2012 - 2018 Patrick Schleizer <adrelanos@riseup.net>
 ## See the file COPYING for copying conditions.
 
 import sys
